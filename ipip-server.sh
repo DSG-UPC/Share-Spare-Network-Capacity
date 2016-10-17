ae271792e0e6de2d9bcb3c788be792eaab0730f5
Roc Meseguer <meeguer@ac.upc.edu>
2016-10-17 14:04:36 +0200

    IPoverIP

---
+++ ipip-server.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+modprobe ipip
+
+sudo ip tunnel add ipiptun1 mode ipip 147.83.118.24 remote 147.83.118.124 ttl 64
+sudo ip link set dev ipiptun1 up
+sudo ip addr add 192.168.0.1/24 dev ipiptun1
+
+
