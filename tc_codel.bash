5f222c1f07c8e7ba386b125e0cd1cbec0af07aec
rocmeseguer <meseguer@ac.upc.edu>
2016-10-20 12:24:09 +0200

    CODEL setup

---
+++ tc_codel.bash
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+#   http://manpages.ubuntu.com/manpages/xenial/en/man8/tc-fq_codel.8.html
+#   https://www.bufferbloat.net/projects/codel/wiki/RRUL_Rogues_Gallery/
+#   http://netseminar.stanford.edu/seminars/Inside_Codel_and_Fq_Codel.pdf
+#
+
+TC=/sbin/tc
+IF=eno1		    # Interface 
+ 
+start() {
+    $TC qdisc add dev $IF root fq_codel
+    show
+}
+
+stop() {
+
+    $TC qdisc del dev $IF root
+
+}
+
+restart() {
+
+    stop
+    sleep 1
+    start
+
+}
+
+show() {
+
+    $TC -s qdisc ls dev $IF
+
+}
+
+case "$1" in
+
+  start)
+
+    echo -n "Starting CODEL shaping: "
+    start
+    echo "done"
+    ;;
+
+  stop)
+
+    echo -n "Stopping CODEL shaping: "
+    stop
+    echo "done"
+    ;;
+
+  restart)
+
+    echo -n "Restarting CODEL shaping: "
+    restart
+    echo "done"
+    ;;
+
+  show)
+    	    	    
+    echo "qdisc status for $IF:\n"
+    show
+    echo ""
+    ;;
+
+  *)
+
+    pwd=$(pwd)
+    echo "Usage: $(/usr/bin/dirname $pwd)/tc_codel.bash {start|stop|restart|show}"
+    ;;
+
+esac
+
+exit 0
+
