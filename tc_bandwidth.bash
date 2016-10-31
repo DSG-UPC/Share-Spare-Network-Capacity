f7b63790813f686ca35d95a4eb16de2c0eae9acb
rocmeseguer <meseguer@ac.upc.edu>
2016-10-31 18:38:56 +0100

    Update tc_bandwidth.bash

--- tc_bandwidth.bash
+++ tc_bandwidth.bash
@@ -15,25 +15,34 @@
 #
 TC=/sbin/tc
 IF=eno1		    # Interface 
-IP=216.3.128.12     # Host IP
 
 # Modelado basado en [10]
 # Download throughput (Mbps), Upload throughput (Mbps) andLatency - RTT (ms)
 # guifi.net 9.78, 7.82, 14
 # Telefonica 1.72, 0.54, 74
 
-DNLD=1.72mbps          # DOWNLOAD Limit
+DNLD=1.72mbit          # DOWNLOAD Limit
 LATENCY=74ms
 
 start() {
 
 # https://linux.die.net/man/8/tc-tbf
+
+# Thoughtput
 #	$TC qdisc add dev $IF root tbf rate $DNLD latency 50ms burst 1540
 
-# https://linux.die.net/man/8/tc-cbq
-#   $TC qdisc add $IF root cbq bandwidth $DNLD avpkt 1000
-    $TC qdisc add dev $IF root handle 1:0 netem delay $LATENCY
-	$TC qdisc add dev $IF parent 1:1 handle 10: cbq bandwidth $DNLD avpkt 1000
+# Thoughtput and delay
+#   $TC qdisc add dev $IF root handle 1:0 tbf rate $DNLD latency 50ms burst 1540
+#   $TC qdisc add dev $IF parent 1:1 handle 10:0 netem delay $LATENCY
+
+
+# https://linux.die.net/man/8/tc-htb
+
+# Thoughtput and delay
+   $TC qdisc add dev $IF root handle 1: htb
+   $TC class add dev $IF parent 1:0 classid 1:1 htb  rate $DNLD
+   $TC qdisc add dev $IF parent 1:1 handle 10:0 netem  delay $LATENCY
+   $TC filter add dev $IF parent 1: protocol ip prio 1 u32 match ip dst 0.0.0.0/0 flowid 1:1
 }
 
 stop() {
