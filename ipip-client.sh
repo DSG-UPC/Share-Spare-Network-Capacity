95330f11081a7209372d6b4f21977621b3a5e6a3
Manos <emmdim@users.noreply.github.com>
2016-11-02 12:42:08 +0100

    Fixes wrong router ip address

--- ipip-client.sh
+++ ipip-client.sh
@@ -6,9 +6,8 @@
 
 modprobe ipip
 
-sudo ip tunnel add ipiptun1 mode ipip local 147.83.118.126 remote 147.83.118.126 ttl 64
+sudo ip tunnel add ipiptun1 mode ipip local 147.83.118.126 remote 147.83.118.124 ttl 64
 sudo ip link set dev ipiptun1 up
 sudo ip addr add 192.168.0.2/24 dev ipiptun1
-
 
 
