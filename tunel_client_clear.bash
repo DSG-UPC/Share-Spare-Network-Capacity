c00c793ea17fa32bb7fc44b30eaef04faf88201f
Manos <savemanos@gmail.com>
2016-11-21 18:04:29 +0100

    Changes for moving tunels from server-router to router-client -New fabfile -New scripts that are executed on the client to provide different gateway according to destination port

---
+++ tunel_client_clear.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#https://aftermanict.blogspot.com.es/2015/11/bash-iptables-iproute2-and-multiple.html
+#http://themediaserver.com/bypass-vpn-connections-specifics-ports-ubuntu-kodibuntu/
+
+
+
+TUNINT=$1
+
+sudo iptables -D OUTPUT -t mangle -p tcp --dport 80 -j MARK --set-mark 1
+sudo iptables -D OUTPUT -t mangle -p tcp --dport 8080 -j MARK --set-mark 2
+sudo ip rule del from all fwmark 1 table 1
+sudo ip rule del from all fwmark 2 table 2
+sudo ip route flush cache
+# A default route should exist in the main routing table as well
+#sudo ip route add default via 127.0.1.1
+sudo ip route flush table primary
+sudo ip route flush table secondary
+sudo iptables -t nat -D POSTROUTING -o enp0s25 -j MASQUERADE
+sudo iptables -t nat -D POSTROUTING -o $TUNINT -j MASQUERADE
